75e1cd67a3460338c99dc13e2fe93caa8ba27e77
AN-katg <80961115+AN-katg@users.noreply.github.com>
2021-06-09 11:13:44 +0800

    Add files via upload

---
+++ book01.js
@@ -0,0 +1,25 @@
+// var img=document.getElementsByTagName("img")[3];
+// var i=1;
+// var time = setTimeout("showImg()", 2000);
+// function icon_left(){
+//     clearTimeout(time)
+//     if(i<=1){
+//         i=4;
+//     }else{
+//         i=i-2;
+//     }
+//     console.info(i)
+//     showImg()
+// }
+// function icon_right(){
+//     clearTimeout(time)
+//     showImg()
+// }
+// function showImg() { 
+//     i++;
+//     if(i == 6) {
+//         i = 1;
+//     }
+//     img.src="./picture/lun"+i+".jpg";
+//     time = setTimeout("showImg()", 1500);
+// }
